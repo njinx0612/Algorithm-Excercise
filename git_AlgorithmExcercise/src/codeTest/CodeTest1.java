ecced3928f20107dcd43a2973b99b4184085972c
dongki <dongki@218.38.137.27>
2020-12-19 12:13:17 +0900

    Coding Excercise

--- git_AlgorithmExcercise/src/codeTest/CodeTest1.java
+++ git_AlgorithmExcercise/src/codeTest/CodeTest1.java
@@ -3,9 +3,20 @@ package codeTest;
 public class CodeTest1 {
 
 	public static void main(String[] args) {
+		System.out.println("피보나치 수열 시작");
+		
 		for(int i=0; i<10; i++) {
-			System.out.println(i%5);
+//			System.out.println(fibonachi(i));
 		}
+		System.out.println(fibonachi(10));
 	}
-
+	
+	private static int fibonachi(int cnt) {
+		
+		if(cnt <= 0) return 0;
+		if(cnt == 1) return 1;
+	
+		return fibonachi(cnt-2)+fibonachi(cnt-1);
+	}
+	
 }
