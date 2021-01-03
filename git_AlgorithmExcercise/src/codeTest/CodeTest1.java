c496469170f3d9f4e415d1eba8a34c988f6bc9fe
dongki <dongki@218.38.137.27>
2021-01-03 10:36:31 +0900

    recursive -> Dynamic Algorithm

--- git_AlgorithmExcercise/src/codeTest/CodeTest1.java
+++ git_AlgorithmExcercise/src/codeTest/CodeTest1.java
@@ -1,22 +1,33 @@
 package codeTest;
 
-public class CodeTest1 {
+import java.io.File;
+import java.io.IOException;
 
+public class CodeTest1 {
+	static int callCnt = 0;
+	static int[] fiboArr;
 	public static void main(String[] args) {
 		System.out.println("피보나치 수열 시작");
 		
-		for(int i=0; i<10; i++) {
-//			System.out.println(fibonachi(i));
+		fiboArr = new int [100];
+		for(int i=0; i<=15; i++) {
+			System.out.println("fibo = "+fibonachi(i));
 		}
-		System.out.println(fibonachi(10));
+		
+//		File f = new File("NewFile.java");
+//		System.out.println(f.exists());
+//		System.out.println(f.isDirectory());
 	}
 	
 	private static int fibonachi(int cnt) {
-		
+		callCnt++;
+		System.out.println("call Cnt = " + callCnt);
 		if(cnt <= 0) return 0;
 		if(cnt == 1) return 1;
-	
-		return fibonachi(cnt-2)+fibonachi(cnt-1);
+		if(fiboArr[cnt] != 0) return fiboArr[cnt];
+		else return fiboArr[cnt] = fibonachi(cnt-2)+fibonachi(cnt-1); 
+
+//		return fiboArr[cnt] = fibonachi(cnt-2)+fibonachi(cnt-1);
 	}
 	
 }
